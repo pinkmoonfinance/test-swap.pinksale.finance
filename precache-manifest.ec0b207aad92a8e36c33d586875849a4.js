bb91da5c85d488e3a703498dee565b12c39b9aa7
PinkMoon <82945665+pinkmoonfinance@users.noreply.github.com>
2021-09-02 20:45:08 +0700

    Add files via upload

---
+++ precache-manifest.ec0b207aad92a8e36c33d586875849a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01246a844de39ce71a3ab79eed8a4abd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8936f77922c4ecbafbf",
+    "url": "/static/js/2.179d120b.chunk.js"
+  },
+  {
+    "revision": "8efadbe79de939f183de4c88d2491df0",
+    "url": "/static/js/2.179d120b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "848c17e35a4071be56e4",
+    "url": "/static/js/main.d1aadb26.chunk.js"
+  },
+  {
+    "revision": "551c9e280db88d76f240",
+    "url": "/static/js/runtime-main.c307dd36.js"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "/static/media/eth.73dabb37.png"
+  },
+  {
+    "revision": "ceb649b353c4f80233d97a75b9ba6a15",
+    "url": "/static/media/logo.ceb649b3.png"
+  }
+]);
